58f134b20cac7d48ec479c90a9e34c04c3dc49ab
artbysekai <projectmisery.grrr@gmail.com>
2026-08-15 03:42:57 +0200

    Update label text for HouseArrest navigation

--- mond/views/App/ContentView.swift
+++ mond/views/App/ContentView.swift
@@ -42,7 +42,7 @@ struct ContentView: View {
                     NavigationLink {
                         SantanderView()
                     } label: {
-                        Text("HouseArrest")
+                        Text("HouseArrest(UNSAFE)")
                     }
                     .disabled(false)
                 } header: {
